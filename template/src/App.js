b4f4160993b56ae2618ceed89ca471a53545d35e
Jujhaar Singh Aidhen <146401072+JSA-Masterchief@users.noreply.github.com>
2024-06-15 17:00:05 +0530

    App.js

--- template/src/App.js
+++ template/src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BpkCode } from '@skyscanner/backpack-web/bpk-component-code';
 import BpkButton from '@skyscanner/backpack-web/bpk-component-button';
 import BpkText from '@skyscanner/backpack-web/bpk-component-text';
+import { BpkCalendar, BpkCalendarDate, CALENDAR_SELECTION_TYPE } from 'bpk-component-calendar';
 
 import { cssModules } from '@skyscanner/backpack-web/bpk-react-utils';
 
@@ -9,20 +10,36 @@ import STYLES from './App.scss';
 
 const getClassName = cssModules(STYLES);
 
-const App = () => (
-  <div className={getClassName('App')}>
-    <header className={getClassName('App__header')}>
-      <div className={getClassName('App__header-inner')}>
-        <BpkText tagName="h1" textStyle="xxl" className={getClassName('App__heading')}>Welcome to React + Backpack</BpkText>
-      </div>
-    </header>
-    <main className={getClassName('App__main')}>
-      <BpkText tagName="p" className={getClassName('App__text')}>
-        To get started, edit <BpkCode>src/App.jsx</BpkCode> and save to reload.
-      </BpkText>
-      <BpkButton onClick={() => alert('It works!')}>Click me</BpkButton>
-    </main>
-  </div>
-);
+const App = () => {
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  const handleDateSelect = date => {
+    setSelectedDate(date);
+  };
+
+  return (
+    <div className={getClassName('App')}>
+      <header className={getClassName('App__header')}>
+        <div className={getClassName('App__header-inner')}>
+          <BpkText tagName="h1" textStyle="xxl" className={getClassName('App__heading')}>
+            Flight Schedule
+          </BpkText>
+        </div>
+      </header>
+      <main className={getClassName('App__main')}>
+        <BpkText tagName="p" className={getClassName('App__text')}>
+          To get started, edit <BpkCode>src/App.jsx</BpkCode> and save to reload.
+        </BpkText>
+        <BpkCalendar
+          id="calendar"
+          onDateSelect={handleDateSelect}
+          selectionType={CALENDAR_SELECTION_TYPE.single}
+          date={selectedDate}
+        />
+        <BpkButton onClick={() => alert('It works!')}>Continue</BpkButton>
+      </main>
+    </div>
+  );
+};
 
 export default App;
